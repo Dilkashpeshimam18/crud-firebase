2dc5ef5bb2bfc063a4e48c788bcbd68de169c62b
Dilkash <dilkashpeshimam@gmail.com>
2022-11-14 20:15:41 +0530

    created crud functionality using firestore

--- src/App.js
+++ src/App.js
@@ -1,9 +1,82 @@
+import React, { useState, useEffect } from 'react';
+import { collection, getDocs, addDoc, doc, updateDoc, deleteDoc } from 'firebase/firestore';
 import './App.css';
+import { db } from './firebase/firebase';
 
 function App() {
+  const [users, setUsers] = useState([])
+  const [name, setName] = useState('')
+  const [age, setAge] = useState(0)
+  const usersCollectionRef = collection(db, "users")
+
+  const createUser = async () => {
+    try {
+      const newUser = {
+        name: name,
+        age: Number(age)
+      }
+      await addDoc(usersCollectionRef, newUser)
+
+    } catch (err) {
+      console.log(err)
+    }
+
+  }
+
+  const increaseAge = async (id, age) => {
+    try {
+      const userDoc = doc(db, 'users', id)
+      const newField = { age: age + 1 }
+      await updateDoc(userDoc, newField)
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  const deleteUser = async (id) => {
+    try {
+      const userDoc = doc(db, 'users', id)
+      await deleteDoc(userDoc)
+
+    } catch (err) {
+      console.log(err)
+    }
+  }
+  useEffect(() => {
+
+    const getUsers = async () => {
+      try {
+        const data = await getDocs(usersCollectionRef)
+        const res = data.docs.map((doc) => ({
+          ...doc.data(),
+          id: doc.id
+        }))
+        console.log(res)
+        setUsers(res)
+      } catch (err) {
+        console.log(err)
+      }
+
+    }
+    getUsers()
+
+  }, [])
   return (
     <div className="App">
       <h1>Firebase crud</h1>
+      <div>
+        <input placeholder='Enter your name' value={name} onChange={(e) => setName(e.target.value)} />
+        <input type='number' placeholder='Enter age' value={age} onChange={(e) => setAge(e.target.value)} />
+        <button onClick={createUser}>Create User</button>
+      </div>
+      {users.map((user) => {
+        return <div>
+          <p>Name:{user.name}</p>
+          <p>Age:{user.age}</p>
+          <button onClick={() => increaseAge(user.id, user.age)}>Increase age</button>
+          <button onClick={() => deleteUser(user.id)}>Delete User</button>
+        </div>
+      })}
     </div>
   );
 }
--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,4 +1,6 @@
 import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore"
+
 const firebaseConfig = {
     apiKey: "AIzaSyCAfj9rxGTKyrdIeCMo7Yxs3vATRueaX3g",
     authDomain: "crud-d96e0.firebaseapp.com",
@@ -10,3 +12,4 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig)
+export const db = getFirestore(app)
