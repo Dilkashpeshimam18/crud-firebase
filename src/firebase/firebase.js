1a54b69058247a040236f48221e01c66637ebdf3
Dilkash <dilkashpeshimam@gmail.com>
2022-11-14 14:40:12 +0530

    initialize firebase app with config

---
+++ src/firebase/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+    apiKey: "AIzaSyCAfj9rxGTKyrdIeCMo7Yxs3vATRueaX3g",
+    authDomain: "crud-d96e0.firebaseapp.com",
+    projectId: "crud-d96e0",
+    storageBucket: "crud-d96e0.appspot.com",
+    messagingSenderId: "355796991849",
+    appId: "1:355796991849:web:b15c0140e9f79250e087f8",
+    measurementId: "G-QFDVC2DWB7"
+};
+
+const app = initializeApp(firebaseConfig)
